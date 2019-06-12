14aaa4affc0ace9469e77123a4d3f30def076adc
Nils Geschwinde <nils.geschwinde@newroom-media.de>
2019-06-12 12:46:21 +0200

    added cors and error handling

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
 const dotenv = require('dotenv');
+const cors = require('cors');
 dotenv.config();
 const app = require('express')();
 const request = require('request');
 
+app.use(cors());
+
 app.use('/', (req, res) => {
-    var parts = req.url.split('?');
-    var queryString = parts[1];
-    var updatedPath = parts[0];
-    const path = (updatedPath + '?key=' + process.env.authKey + (queryString ? '&' + queryString : ''));
-    console.log(process.env.baseUrl + path);
-    request(process.env.baseUrl + path, (error, response, body) => {
-        console.log(response.url);
-        if (!error && response.statusCode === 200) {
-            res.contentType(response.headers["content-type"]);
-            //console.log(body);
-            res.send(body);
-        }
-        else {
-            console.log(error);
-            res.sendStatus(404);
-        }
-    });
+    try {
+        var parts = req.url.split('?');
+        var queryString = parts[1];
+        var updatedPath = parts[0];
+        const path = (updatedPath + '?key=' + process.env.authKey + (queryString ? '&' + queryString : ''));
+        console.log('Forwarding to: ' + process.env.baseUrl + path);
+        request(process.env.baseUrl + path, (error, response, body) => {
+            console.log('Response Status: ' + response.statusCode + ' ' + response.statusMessage);
+            if (!error && response.statusCode === 200) {
+                res.contentType(response.headers["content-type"]);
+                res.send(body);
+            }
+            else if(!error) {
+                res.sendStatus(response.statusCode);
+            }else{
+                console.log(error);
+                res.sendStatus(500);
+            }
+        });
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+
 });
 
 app.listen(process.env.PORT);
